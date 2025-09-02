3729d566a1fb630d841e638af3e4a351b2d4c459
Rehan999-tn <jiwakuasu7251@gmail.com>
2025-09-02 15:53:23 +0700

    Update accounts data

--- account.js
+++ account.js
@@ -1,4 +1,9 @@
 var accountsData = {
+  "1": {
+    "password": "2",
+    "sessionId": "#XDWG1",
+    "createdAt": "2025-09-02T08:53:22.150Z"
+  },
   "xemzz": {
     "password": "12314",
     "sessionId": "#XDWG1",
