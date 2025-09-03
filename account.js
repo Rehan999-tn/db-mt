a5d0d912c79fe4e9fe0eb727f3b2ed393eae77bc
Rehan999-tn <jiwakuasu7251@gmail.com>
2025-09-03 21:39:37 +0700

    Update accounts data

--- account.js
+++ account.js
@@ -1,9 +1,4 @@
 var accountsData = {
-  "pk": {
-    "password": "pk1234",
-    "sessionId": "#LR50Q",
-    "createdAt": "2025-09-03T14:15:03.147Z"
-  },
   "halo": {
     "password": "hall",
     "sessionId": "#LR50Q",
