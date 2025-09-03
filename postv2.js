f60e76984f2f1bc4488580a4d63d71a9258957b3
Rehan999-tn <jiwakuasu7251@gmail.com>
2025-09-03 21:36:51 +0700

    Update posts data

--- postv2.js
+++ postv2.js
@@ -5,28 +5,6 @@ var postsData = {
     "timestamp": "2025-09-02T08:47:48.677Z",
     "comments": {}
   },
-  "post2": {
-    "username": "1",
-    "content": "kontol",
-    "timestamp": "2025-09-02T08:54:19.045Z",
-    "comments": {
-      "comment1": {
-        "username": "taik",
-        "content": "bang",
-        "timestamp": "2025-09-02T08:55:46.608Z"
-      },
-      "comment2": {
-        "username": "1",
-        "content": "hai",
-        "timestamp": "2025-09-02T08:56:12.337Z"
-      },
-      "comment3": {
-        "username": "1",
-        "content": "apa",
-        "timestamp": "2025-09-02T09:45:06.211Z"
-      }
-    }
-  },
   "post3": {
     "username": "taik",
     "content": "p",
