4c4fec1557d04d5cb5cabb360c0b48f21ec876a3
Rehan999-tn <jiwakuasu7251@gmail.com>
2025-09-03 21:39:02 +0700

    Update posts data

--- postv2.js
+++ postv2.js
@@ -1,14 +1,1 @@
-var postsData = {
-  "post4": {
-    "username": "xem",
-    "content": "kontol pecah",
-    "timestamp": "2025-09-03T14:14:48.874Z",
-    "comments": {
-      "comment1": {
-        "username": "pk",
-        "content": "halo",
-        "timestamp": "2025-09-03T14:15:31.024Z"
-      }
-    }
-  }
-};
+var postsData = {};
